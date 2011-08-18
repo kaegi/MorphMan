ca9d218b5fdb265e66add41b5caef4acdf2c8c29
Joseph Re <overture2112@gmail.com>
2011-08-18 13:58:02 -0400

    fixed some bugs, add maturities support, now asks for which field to use

--- anki/plugins/morphology/morph/exportMorphemes.py
+++ anki/plugins/morphology/morph/exportMorphemes.py
@@ -1,20 +1,29 @@
 from PyQt4.QtCore import *
 from PyQt4.QtGui import *
+from anki.utils import ids2str
+from anki.cards import Card
 
-import morphemes as m
+import morphemes as M
 import util
 
+def getCards( deck, fids ):
+    cis = deck.s.column0( 'select id from cards where factId in %s' % ids2str(fids) )
+    cs = [ deck.s.query(Card).get( id ) for id in cis ]
+    return cs
+
 def pre( ed ):
-    name, ok = QInputDialog.getText( ed, 'Enter name of source', 'Name', QLineEdit.Normal, 'recentSelection' )
+    field, ok = QInputDialog.getText( ed, 'Enter name of field to extract from', 'Field Name', QLineEdit.Normal, 'Expression' )
     if not ok: return 'BAIL'
-    defPath = util.dbPath + name + '.db'
+    defPath = util.dbPath + 'mySelection' + '.db'
     path = QFileDialog.getSaveFileName( caption='Save morpheme db to?', directory=defPath )
     if not path: return 'BAIL'
-    return { 'ed':ed, 'srcName':name, 'filePath':path, 'db':m.MorphDb(), 'mp':m.mecab(None) }
+    return { 'ed':ed, 'fieldName':field, 'filePath':str(path), 'db':M.MorphDb(), 'mp':M.mecab() }
 
 def per( st, f ):
-    ms = m.getMorphemes( st['mp'], f[ 'Expression' ] )
-    loc = m.AnkiDeck( f.id, 'Expression', st['ed'].deck.path, st['ed'].deck.name )
+    d, fname = st['ed'].deck, st['fieldName']
+    mats = [ c.interval for c in getCards( d, [f.id] ) ]
+    ms = M.getMorphemes( st['mp'], f[ fname ] )
+    loc = M.AnkiDeck( f.id, str(fname), str(d.path), str(d.name()), mats )
     st['db'].addMsL( ms, loc )
     return st
 
@@ -23,6 +32,6 @@ def post( st ):
     st['db'].save( st['filePath'] )
     wantMerge = QMessageBox.question( st['ed'], 'Query', 'Would you like to merge with known db?', QMessageBox.Yes | QMessageBox.No, QMessageBox.No )
     if wantMerge == QMessageBox.Yes:
-        m.MorphDb.mergeFiles( st['filePath'], util.knownDbPath, util.knownDbPath )
+        M.MorphDb.mergeFiles( st['filePath'], util.knownDbPath, util.knownDbPath )
 
 util.addDoOnSelectionBtn( 'Export Morphemes', 'Morpheme export', 'Exporting...', pre, per, post )
