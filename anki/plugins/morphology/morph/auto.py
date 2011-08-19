3ab2af6cafe2ead9fa3f33de821898c1a6c1d2b7
Joseph Re <overture2112@gmail.com>
2011-08-19 18:50:26 -0400

    overhaul of automatic management system

--- anki/plugins/morphology/morph/auto.py
+++ anki/plugins/morphology/morph/auto.py
@@ -1,24 +1,148 @@
 #-*- coding: utf-8 -*-
 
-import os
+import os, time, datetime, gzip, pickle, stat
 from ankiqt import mw
 from anki.deck import DeckStorage
+from anki.facts import Fact
+from anki.cards import Card
+from anki.hooks import addHook
+from anki.utils import ids2str
+from ankiqt.ui.utils import askUser, showText
+import morphemes as M
 
-deckDbPath = os.sep.join([ mw.pluginsFolder,'morph','dbs','deck' ])
+'''Todo:
+* notify which decks are being updated and which aren't
+'''
 
-def edeck( dpath ):
+rootDeckDbPath = os.path.join( mw.pluginsFolder(),'morph','dbs','deck' )
+logPath = os.path.join( mw.pluginsFolder(),'morph','auto.log' )
+
+# Debugging
+def printf( msg, parent=None, type="text" ):
+    print msg
+    showText( msg, parent, type )
+def log( msg ):
+    with open( logPath, 'a' ) as f:
+        f.write( '%s: %s\n' % (datetime.datetime.now(), msg) )
+
+# Deck load
+def getDeck( dpath ):
     if not os.path.exists( dpath ):
-        print '!! deck file not found:', dpath
-        return False
+        return log( 'deck file not found @ %s' % dpath )
     try:
-        d = DeckStorage.Deck( dpath )
-        d.close()
+        return DeckStorage.Deck( dpath )
     except Exception, e:
         if hasattr( e, 'data' ) and e.data.get('type') == 'inuse':
-            print '!! deck already open:', dpath
+            log( 'deck already open @ %s. skipping' % dpath )
         else:
-            print '!! deck is corrupted:', dpath, e
+            printf( '!! deck is corrupted: %s\nException was: %s' % (dpath, e) )
 
-deckPaths = mw.config['recentDeckPaths']
-for dpath in deckPaths:
-    edeck( dpath )
+class DeckMgr:
+    def __init__( self, deck ):
+        self.deck     = deck
+        self.deckName = str( deck.name() )
+        self.deckPath = str( deck.path )
+        self.dbsPath  = rootDeckDbPath + os.sep + self.deckName
+        self.cfgPath  = self.dbsPath + os.sep + 'config'
+        self.allPath  = self.dbsPath + os.sep + 'all.db'
+        self.maturePath  = self.dbsPath + os.sep + 'mature.db'
+
+        self.cfg = {
+            'mature threshold':21,
+            'learnt threshold':3,
+            'morph fields':['Expression'],
+        }
+        self.loadCfg()
+        log( 'Loaded %s' % self.deckName )
+
+    # Config
+    def loadCfg( self ):
+        log( 'loading cfg' )
+        try:
+            f = gzip.open( self.cfgPath, 'rb' )
+            d = pickle.load( f )
+            self.cfg.update( d )
+            f.close()
+            self.saveCfg()
+        except IOError:
+            log( 'cfg load failed' )
+
+    def saveCfg( self ):
+        log( 'saving cfg' )
+        if not os.path.exists( self.dbsPath ):
+            os.makedirs( self.dbsPath )
+        f = gzip.open( self.cfgPath, 'wb' )
+        pickle.dump( self.cfg, f )
+        f.close()
+
+    # Clean
+    def close( self ):
+        self.saveCfg()
+        self.deck.close()
+
+    # Analysis
+    def getFacts( self ):
+        fids = self.deck.s.column0( 'select id from facts' )
+        return map( self.deck.s.query( Fact ).get, fids )
+
+    def getCardsForFacts( self, fids ):
+        cis = self.deck.s.column0( 'select id from cards where factId in %s' % ids2str(fids) )
+        return map( self.deck.s.query( Card ).get, cis )
+
+    # Update DBs
+    def mkAll( self ):
+        db = M.MorphDb()
+        mp = M.mecab()
+        for f in self.getFacts():
+            mats = [ c.interval for c in self.getCardsForFacts( [f.id] ) ]
+            for fieldName in self.cfg['morph fields']:
+                ms = M.getMorphemes( mp, f[ fieldName ] )
+                loc = M.AnkiDeck( f.id, fieldName, self.deckPath, self.deckName, mats )
+                db.addMsL( ms, loc )
+        db.save( self.allPath )
+        return db
+
+    def getAll( self ):
+        if not os.path.exists( self.allPath ):
+            return self.mkAll()
+        return M.MorphDb( self.allPath )
+
+    def getMature( self ):
+        if not self.isUpToDate( self.maturePath ):
+            return self.mkMature()
+        return M.MorphDb( self.maturePath )
+
+    def mkMature( self ):
+        matDb = M.MorphDb()
+        for l,ms in self.getAll().ldb().iteritems():
+            if l.maturity > self.cfg['mature threshold']:
+                matDb.addMsL( ms, l )
+        matDb.save( self.maturePath )
+        return matDb
+
+    def isUpToDate( self, dbPath ):
+        if not os.path.exists( dbPath ): return False
+        dbMod = os.stat( dbPath )[ stat.ST_MTIME ]
+        if self.deck.modified - dbMod > 0:
+            return False
+        return True
+
+def doDeck( dPath ):
+    deck = getDeck( dPath )
+    if not deck: return
+    d = DeckMgr( deck )
+    m = d.getMature()
+    log(' mature has len %d' % len(m.db) )
+    d.close()
+
+# main
+def main():
+    start = time.time()
+    deckPaths = mw.config['recentDeckPaths']
+    for dpath in deckPaths[:2]:
+        doDeck( dpath )
+    end = time.time()
+    dur = end - start
+    log( 'Auto completed in %d sec' % dur )
+
+addHook( 'init', main )
