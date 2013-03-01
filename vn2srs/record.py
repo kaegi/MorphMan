e4e3da8a19c7afd243654bf9692173e81986540f
Joseph Re <overture2112@gmail.com>
2013-03-01 00:43:03 -0500

    vn2srs fake history - 2Feb 184200

--- vn2srs/record.py
+++ vn2srs/record.py
@@ -10,21 +10,33 @@ import win32ui, win32gui, win32con, win32api
 import sys, os, time
 
 class AudioRecorder:
-    def __init__( self ):
+    def __init__( self, fileId, hwnd ):
         self.pyaudio = p = pyaudio.PyAudio()
         self.waveFile = None
         self.stream = None
 
         # t.wav is audio data from txt[n].time to txt[n+1].time
         self.frames = []
-        self.lastTextTime = None
 
-        # settings
+        # audio settings
         self.device = p.get_default_input_device_info()[ 'index' ]
+        print 'Using sound input device: %s' % p.get_default_input_device_info()[ 'name' ]
         self.fpb = 1024
         self.rate = 44100
         self.channels = 2
         self.format = pyaudio.paInt16
+
+        # screenshots
+        self.gameHwnd = hwnd
+        print 'Using window with handle %d for screenshots' % hwnd
+
+        # file output
+        self.fileId = fileId
+        self.count = 0
+        def mkdirp( p ):
+            try:    os.makedirs( p )
+            except: pass
+        for p in [ 'media/txt','media/img','media/audio', 'media/misc' ]: mkdirp( p )
 
     def saveAudio( self, path ):
         'Save all current self.frames to wave file then clear'
@@ -40,14 +52,13 @@ class AudioRecorder:
         self.running = True
 
         # wave file to write audio to
-        self.waveFile = wf = wave.open( 'all.wav', 'wb' )
+        self.waveFile = wf = wave.open( 'media/misc/all.%s.wav' % self.fileId, 'wb' )
         wf.setnchannels( self.channels )
         wf.setsampwidth( self.pyaudio.get_sample_size( self.format ) )
         wf.setframerate( self.rate )
 
         # timing data
-        self.timingFile = tf = codecs.open( 'timing.txt', 'wb', encoding='utf-16' )
-        #self.timingFile = tf = open( 'timing.txt', 'wb' )
+        self.timingFile = tf = codecs.open( 'media/misc/timing.%s.txt' % self.fileId, 'wb', encoding='utf-16' )
         self.time0 = time.time()
         self.timeData = {}
 
@@ -70,7 +81,7 @@ class AudioRecorder:
         self.waveFile.close()
 
         # timing data
-        pickle.dump( self.timeData, open( 'timing.dat', 'wb' ), -1 )
+        pickle.dump( self.timeData, open( 'media/misc/timing.%s.dat' % self.fileId, 'wb' ), -1 )
         self.timingFile.close()
 
         # remove clipboard hooks
@@ -101,20 +112,38 @@ class AudioRecorder:
         t = time.time() - self.time0    # time since start of audio file
         win32clipboard.OpenClipboard()
         txt = win32clipboard.GetClipboardData( win32clipboard.CF_UNICODETEXT )
+        '''
+        # ideas for cleaning up garbage at end
+        print repr(raw)
+        txt = raw.split( u'\x00\x00' )[0]
+        if txt == raw:
+            txt = txt.split( u'\x00' )[0]
+        
+        # TODO remove everything after string terminator
+        #try:    txt = txt.split( u'\x00\x00' )[0]
+        #except: pass
+        try:
+            txt = win32clipboard.GetClipboardData( win32clipboard.CF_UNICODETEXT )
+            win32clipboard.EmptyClipboard()
+        except TypeError: # no unicode data, probably notification that it was cleared
+            h = self.hPrev
+            if h:   return win32api.SendMessage( h, *args[-1][1:4] )
+            else:   return 1
+        '''
         win32clipboard.CloseClipboard()
 
         # iteratives
-        self.timeData[ t ] = txt
-        takeScreenshot( '%f.bmp' % t )
-        codecs.open( '%f.txt' % t, 'wb', encoding='utf-16' ).write( txt )
+        self.timeData[ self.count ] = ( t, txt )
+        takeScreenshot( 'media/img/{id}.{n}.bmp'.format( id=self.fileId, n=self.count ), self.gameHwnd )
+        codecs.open( 'media/txt/{id}.{n}.txt'.format( id=self.fileId, n=self.count ), 'wb', encoding='utf-16' ).write( txt )
         # save audio for previous text
-        if self.lastTextTime:
-            self.saveAudio( '%f.wav' % self.lastTextTime )
-            self.lastTextTime = t
+        if self.count > 0:
+            self.saveAudio( 'media/audio/{id}.{n}.wav'.format( id=self.fileId, n=self.count-1 ) )
+        self.count += 1
 
         # big time & txt log
         print t
-        line = u'%f %s\r\n' % ( t, txt )
+        line = u'{n} {t} {txt}\r\n'.format( n=self.count, t=t, txt=txt )
         self.timingFile.write( line )
 
         h = self.hPrev
@@ -139,8 +168,11 @@ class AudioRecorder:
         except win32api.error, err:
             if err.args[0]: raise
 
-def takeScreenshot( path='screenshot.bmp' ):
-    hwnd = win32gui.GetForegroundWindow()
+def takeScreenshot( path='screenshot.bmp', hwnd=None ):
+    if hwnd is None:
+        time.sleep(3)
+        hwnd = win32gui.GetForegroundWindow()
+        return hwnd
     l, t, r, b = win32gui.GetWindowRect( hwnd )
     w = r - l
     h = b - t
@@ -156,7 +188,10 @@ def takeScreenshot( path='screenshot.bmp' ):
     bmp.SaveBitmapFile( saveDC, path )
 
 def main():
-    ar = AudioRecorder()
-    #ar.printDevices()
+    if len( sys.argv ) < 2:
+        print 'Please specify an identifier to use in file names (for ordering/resuming)'
+        return
+    print 'Please switch to the game window within 3 seconds in order to identify it for screenshot purposes'
+    ar = AudioRecorder( sys.argv[1], takeScreenshot() )
     ar.loop()
 main()
