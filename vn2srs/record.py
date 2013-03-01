a198bee19f45c1407d564cb25820847dbfe79cb9
Joseph Re <overture2112@gmail.com>
2013-03-01 00:41:00 -0500

    vn2srs fake history 2Feb 040000

--- vn2srs/record.py
+++ vn2srs/record.py
@@ -1,15 +1,13 @@
 #!/usr/bin/env python
 #-*- coding: utf-8 -*-
 
+import cPickle as pickle
+import codecs
 import pyaudio
 import wave
 import win32clipboard
 import win32ui, win32gui, win32con, win32api
 import sys, os, time
-
-################################################################################
-## Audio recording
-################################################################################
 
 class AudioRecorder:
     def __init__( self ):
@@ -19,7 +17,6 @@ class AudioRecorder:
 
         # settings
         self.device = p.get_default_input_device_info()[ 'index' ]
-        #self.device = p.get_default_output_device_info()[ 'index' ]
         self.fpb = 1024
         self.rate = 44100
         self.channels = 2
@@ -27,26 +24,51 @@ class AudioRecorder:
 
     def start( self ):
         self.running = True
+
+        # wave file to write audio to
         self.waveFile = wf = wave.open( 'output.wav', 'wb' )
         wf.setnchannels( self.channels )
         wf.setsampwidth( self.pyaudio.get_sample_size( self.format ) )
         wf.setframerate( self.rate )
 
+        # timing data
+        self.timingFile = tf = codecs.open( 'timing.txt', 'wb', encoding='utf-16' )
+        #self.timingFile = tf = open( 'timing.txt', 'wb' )
+        self.time0 = time.time()
+        self.timeData = {}
+
+        # async audio input stream
         self.stream = stream = self.pyaudio.open( format=self.format, channels=self.channels, rate=self.rate, input=True, frames_per_buffer=self.fpb, input_device_index=self.device, stream_callback=self.onAudioInput )
         stream.start_stream()
 
+        # register clipboard monitor
+        self.registerClipboardCallback()
+
     def stop( self ):
         self.running = False
+
+        # async audio input stream
         while self.stream.is_active():
             self.stream.stop_stream()
         self.stream.close()
+
+        # wave file being written
         self.waveFile.close()
+
+        # timing data
+        pickle.dump( self.timeData, open( 'timing.dat', 'wb' ), -1 )
+        self.timingFile.close()
+
+        # remove clipboard hooks
+        win32clipboard.ChangeClipboardChain( self.window.GetSafeHwnd(), self.hPrev )
+        self.window.DestroyWindow()
 
     def loop( self ):
         self.start()
         try:
             while self.stream.is_active():
-                time.sleep( 0.1 )
+                win32gui.PumpWaitingMessages()
+                #time.sleep( 0.1 )
         finally:
             self.stop()
 
@@ -56,72 +78,46 @@ class AudioRecorder:
             return  ( None, pyaudio.paComplete )
         return ( None, pyaudio.paContinue )
 
-    def devices( self ):
+    def printDevices( self ):
         for i in xrange( 0, self.pyaudio.get_device_count() ):
             print i, self.pyaudio.get_device_info_by_index( i )
 
-################################################################################
-## Clipboard monitor
-################################################################################
-hPrev = 0
-nCbcs = 0
+    def onDrawClipboard( self, *args ):
+        t = time.time() - self.time0    # time since start of audio file
+        win32clipboard.OpenClipboard()
+        txt = win32clipboard.GetClipboardData( win32clipboard.CF_UNICODETEXT )
+        win32clipboard.CloseClipboard()
 
-def onDrawCb( *args ):
-    global nCbcs
-    t = time.time()
-    nCbcs += 1
-    #print 'onDrawCb', nCbcs, args
-    win32clipboard.OpenClipboard()
-    txt = win32clipboard.GetClipboardData()
-    win32clipboard.CloseClipboard()
-    print t, txt
-    if hPrev: return win32api.SendMessage( hPrev, *args[-1][1:4] )
-    else: return 1
+        self.timeData[ t ] = txt
 
-def onCcbc( *args ):
-    global hPrev
-    #print 'onCcbc', args
-    if hPrev == args[-1][2]: hPrev = a[-1][3]
-    elif hPrev: return win32api.SendMessage( hPrev, *a[-1][1:4] )
-    else: return 0
+        print t
+        line = u'%f %s\r\n' % ( t, txt )
+        self.timingFile.write( line )
 
-def wait(timeout_msec=5000, need_changes=1):
-    global hPrev, nCbcs
-    win=win32ui.CreateFrame()
-    win.CreateWindow(None,'',win32con.WS_OVERLAPPEDWINDOW)
-    win.HookMessage(onDrawCb,win32con.WM_DRAWCLIPBOARD)
-    win.HookMessage(onCcbc,win32con.WM_CHANGECBCHAIN)
-    try:
-        hPrev=win32clipboard.SetClipboardViewer(win.GetSafeHwnd())
-    except win32api.error, err:
-        if err.args[0]: raise
-    nCbcs=0
-    if timeout_msec:
-        timid = win.SetTimer(1,20)
-        curtime = win32api.GetTickCount()
-        endtime = timeout_msec + curtime
+        h = self.hPrev
+        if h:   return win32api.SendMessage( h, *args[-1][1:4] )
+        else:   return 1
 
-    while nCbcs<need_changes:
-        win32gui.PumpWaitingMessages()
-        if timeout_msec and win32api.GetTickCount()>endtime: break
+    def onChangeClipboardChain( self, *args ):
+        h = self.hPrev
+        if h == args[-1][2]:    h = a[-1][3]
+        elif h:                 return win32api.SendMessage( h, *args[-1][1:4] )
+        else:                   return 0
 
-    if timeout_msec:
-        win.KillTimer(timid)
-    win32clipboard.ChangeClipboardChain(win.GetSafeHwnd(), hPrev)
-    win.DestroyWindow()
-    return nCbcs
+    def registerClipboardCallback( self ):
+        self.hPrev = None
 
-# start clipboard daemon
-#wait( timeout_msec=None, need_changes = sys.maxint )
-
-################################################################################
-## Driver
-################################################################################
+        self.window = win = win32ui.CreateFrame()
+        win.CreateWindow( None, '', win32con.WS_OVERLAPPEDWINDOW )
+        win.HookMessage( self.onDrawClipboard, win32con.WM_DRAWCLIPBOARD )
+        win.HookMessage( self.onChangeClipboardChain, win32con.WM_CHANGECBCHAIN )
+        try:
+            self.hPrev = win32clipboard.SetClipboardViewer( win.GetSafeHwnd() )
+        except win32api.error, err:
+            if err.args[0]: raise
 
 def main():
     ar = AudioRecorder()
-    #ar.devices()
-    #ar.loop()
-    ar.start()
-    #wait( None, sys.maxint )
+    #ar.printDevices()
+    ar.loop()
 main()
