2357c16c4f0a663581fdca0dfd396a15f5ecc383
Joseph Re <overture2112@gmail.com>
2013-03-24 02:59:09 -0400

    counterpart lines don't always have same offset, so parse differently

--- vn2srs/mkMuvLuvTranslationDb.py
+++ vn2srs/mkMuvLuvTranslationDb.py
@@ -1,32 +1,41 @@
 #!/usr/bin/env python
 #-*- coding: utf-8 -*-
 import codecs
+import cPickle as pickle
 import glob
 import os
 import re
 
 ps = glob.glob( 'mla_scripts/MLA_*' )
-engDb, japDb, jap2eng = {}, {}, {}
+jap2eng = {}
+dupes = []
 for p in ps:
     name = os.path.basename( p ).split('MLA_')[1].split('.txt')[0]
     print 'Processing', name
     lines = codecs.open( p, 'rb', encoding='shift-jis', errors='ignore' ).read().split(u'\r\n')
     d = dict( (i,line) for i,line in enumerate(lines) )
+
+    japLine, engLine = None, None
     for lineno,line in d.items():
         r = re.match( '//<(.*?)> (.*)', line )
-        if r: japDb[ name + r.group(1) ] = r.group(2)
+        if r:
+            japLine = r.group(2)
 
         r = re.match( '<(.*?)> (.*)', line )
-        if r: engDb[ name + r.group(1) ] = r.group(2)
+        if r:
+            engLine = r.group(2)
 
-print 'engDb:', len( engDb )
-print 'japDb:', len( japDb )
-for k in japDb:
-    try: jap2eng[ japDb[k] ] = engDb[k]
-    except KeyError:
-        print 'Failed to match'
-        print k
-        print japDb.get( k )
-        print engDb.get( k )
-        raise
-print 'jap2eng:', len( jap2eng )
+        if japLine and engLine:
+            if japLine in jap2eng:
+                dupes.append( ( japLine, engLine ) )
+            jap2eng[ japLine ] = engLine
+            japLine, engLine = None, None
+pickle.dump( jap2eng, open( 'muvluv_mla.db', 'wb' ) )
+'''
+i = 0
+for j,e in dupes:
+    if j == u'\\r': continue
+    print j, e
+    i += 1
+print len( jap2eng ), len( dupes ), i
+'''
