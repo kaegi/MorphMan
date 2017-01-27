8600484bc5389130271b26498eefc6e2d711c062
Chang Spivey <chang.spivey@gmail.com>
2017-01-27 23:57:19 +0100

    Add type assertion to help jedi autocompletion

--- morph/util.py
+++ morph/util.py
@@ -10,6 +10,10 @@ from aqt.utils import showCritical, showInfo, showWarning, tooltip
 from anki.hooks import addHook, wrap
 from util_external import memoize
 
+# only for jedi-auto-completion
+import aqt.main
+assert isinstance(mw, aqt.main.AnkiQt)
+
 ###############################################################################
 ## Global data
 ###############################################################################
