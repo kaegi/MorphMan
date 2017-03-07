d9ed0384c9cd451cd43754407b16baa534e9110d
Greg Price <gnprice@gmail.com>
2017-03-07 00:10:33 -0800

    cli: Add --profile option to explicitly point to the Anki profile
    This allows a user who has several profiles, or who has their Anki
    folder in a different place from our couple of crude guesses (e.g.,
    because their desktop's "documents" folder is at a path in a language
    other than English), to use the CLI.
    
    It'd still be better to actually have the complete logic for where to
    find the Anki folder, so that we can do that much for all users in the
    first place, but this is a good start by providing a simple workaround.

--- morph/cli.py
+++ morph/cli.py
@@ -18,8 +18,13 @@ def warn(msg):
     print >>sys.stderr, msg
 
 
+CLI_PROFILE_PATH = None
+
+
 def profile_base_path():
     '''Dies if we can't find it.'''
+    if CLI_PROFILE_PATH is not None:
+        return os.path.dirname(CLI_PROFILE_PATH)
 
     # To do this right we need, at a minimum, the logic in
     # the method aqt.profiles.ProfileManager._defaultBase .
@@ -35,12 +40,24 @@ def profile_base_path():
         if os.path.exists(d):
             return d
 
-    die('This script is too naive to find your Anki folder.  Tried:\n'
-        + '\n'.join('  %s' % (d,) for d in candidates))
+    die('''\
+This script is too naive to find your Anki folder.  Tried:
+  %s
+Try passing the profile folder explicitly with `--profile`.
+''' % ('\n  '.join(candidates)))
 
 
 def profile_path():
     '''Look for the Anki profile.  Dies unless it finds exactly one.'''
+    if CLI_PROFILE_PATH is not None:
+        path = CLI_PROFILE_PATH
+        if not os.path.isdir(path):
+            die('No such folder at Anki profile path (from --profile): ' + path)
+        dbs_path = os.path.join(path, 'dbs')
+        if not os.path.isdir(dbs_path):
+            die('No MorphMan dbs folder in Anki profile (from --profile): ' + dbs_path)
+        return path
+
     base = profile_base_path()
 
     pattern = os.path.join(base, '*', 'dbs', '')
@@ -83,6 +100,7 @@ def main():
     fix_sigpipe()
 
     parser = argparse.ArgumentParser()
+    parser.add_argument('--profile', metavar='DIR', help='path to Anki profile (if absent, we try some guesses)')
     subparsers = parser.add_subparsers(title='subcommands')
 
     p_dump = subparsers.add_parser('dump', help='dump a MorphMan db in text form',
@@ -91,4 +109,7 @@ def main():
     p_dump.add_argument('name', metavar='NAME', help='database to dump (all, known, ...)')
 
     args = parser.parse_args()
+    global CLI_PROFILE_PATH
+    if args.profile is not None:
+        CLI_PROFILE_PATH = os.path.expanduser(os.path.normpath(args.profile))
     args.action(args)
