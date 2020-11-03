0a8b974569dabfd460096d4fe944a760d7a6ddfd
Joe Strong <joestrong@gmail.com>
2020-11-03 20:26:09 +0000

    Fixes batch play feature in Anki 2.1.26

--- morph/browser/batchPlay.py
+++ morph/browser/batchPlay.py
@@ -1,10 +1,11 @@
 # -*- coding: utf-8 -*-
-import anki.sound
 from anki.hooks import addHook
+from aqt.sound import av_player
 from ..util import addBrowserNoteSelectionCmd
 from ..preferences import get_preference as cfg
 import re
 
+soundReg = r"\[sound:(.+?)\]"
 
 def pre(b): return {'vid2nid': {}}
 
@@ -12,7 +13,7 @@ def pre(b): return {'vid2nid': {}}
 def per(st, n):
     for f in cfg('batch media fields', n.mid):
         try:
-            r = re.search(anki.sound._soundReg, n[f])
+            r = re.search(soundReg, n[f])
             if r:
                 st['vid2nid'][r.group(1)] = n.id
                 break
@@ -25,7 +26,7 @@ def post(st):
     # TODO: queue all the files in a big list with `loadfile {filename} 1` so you can skip back and forth easily
     # when user chooses, use `get_file_name`
     for vid, nid in st['vid2nid'].items():
-        anki.sound.play(vid)
+        av_player.insert_file(vid)
     st['__reset'] = False
     return st
 
