1deb9029afcafdd987c303276b8b20048eb40721
ianki <iankigit@gmail.com>
2022-01-16 12:56:56 -0800

    Impove performance of morpheme caching.

--- morph/morphemizer.py
+++ morph/morphemizer.py
@@ -1,35 +1,11 @@
 # -*- coding: utf-8 -*-
 import re
+from functools import lru_cache
 
 from .morphemes import Morpheme
 from .deps.zhon.hanzi import characters
 from .mecab_wrapper import getMorphemesMecab, getMecabIdentity
 from .deps.jieba import posseg
-
-class LRUCache:
-    def __init__(self, capacity):
-        self.capacity = capacity
-        self.tm = 0
-        self.cache = {}
-        self.lru = {}
-
-    def get(self, key):
-        if key in self.cache:
-            self.lru[key] = self.tm
-            self.tm += 1
-            return self.cache[key]
-        return None
-
-    def set(self, key, value):
-        if len(self.cache) >= self.capacity:
-            # find the LRU entry
-            old_key = min(self.lru.keys(), key=lambda k:self.lru[k])
-            self.cache.pop(old_key)
-            self.lru.pop(old_key)
-        self.cache[key] = value
-        self.lru[key] = self.tm
-        self.tm += 1
-
 
 ####################################################################################################
 # Base Class
@@ -37,17 +13,13 @@ class LRUCache:
 
 class Morphemizer:
     def __init__(self):
-        self.lru = LRUCache(1000000)
-        
+        pass
+    
+    @lru_cache(maxsize=131072)
     def getMorphemesFromExpr(self, expression):
         # type: (str) -> [Morpheme]
-
-        morphs = self.lru.get(expression)
-        if morphs:
-            return morphs
         
         morphs = self._getMorphemesFromExpr(expression)
-        self.lru.set(expression, morphs)
         return morphs
     
     def _getMorphemesFromExpr(self, expression):
