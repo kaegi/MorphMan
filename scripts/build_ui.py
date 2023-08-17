0c21a9012ec7263ca3ada2476d5479b4f4733ec1
Landon Epps <landon.epps@gmail.com>
2023-08-17 03:12:45 -0400

    Add Qt5 backwards compatibility for UI generation (#303)
    Attempts to fully support PyQt5 backwards compatibility based on feedback from @PrentissLiu

--- scripts/build_ui.py
+++ scripts/build_ui.py
@@ -1,14 +1,21 @@
 import subprocess
 
-
 def build_ui(in_file, out_file):
-    stdout = subprocess.run(["pyuic6", in_file], stdout=subprocess.PIPE).stdout
+    stdout = subprocess.Popen(["pyuic6", in_file], stdout=subprocess.PIPE, text=True).stdout
 
-    lines = stdout.decode("utf-8").replace("__relpath__", "")
-
-    with open(out_file, "w") as sources:
-        sources.write(lines)
-
+    with open(out_file, 'w') as sources:
+        for line in iter(stdout.readline, ""):
+            if line.startswith("from PyQt6 import"):
+                imports = line.split("import")[1].strip()
+                sources.write(f"""try:
+    from PyQt6 import {imports}
+except:
+    from PyQt5 import {imports}
+    QtCore.Qt.AlignmentFlag.AlignLeading = QtCore.Qt.AlignLeading
+    QtCore.Qt.AlignmentFlag.AlignTrailing = QtCore.Qt.AlignTrailing
+""")
+            else:
+                sources.write(line)
 
 build_ui("morph/readability.ui", "morph/readability_ui.py")
 build_ui("morph/readability_settings.ui", "morph/readability_settings_ui.py")
